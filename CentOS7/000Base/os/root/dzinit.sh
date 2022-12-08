d11e4c3cd09126de19c1a5732e19f7bcd10fbdd6
Dylan Zhang <987883389@qq.com>
2022-12-09 03:30:02 +0800

    refactor: rm -f

--- CentOS7/000Base/os/root/dzinit.sh
+++ CentOS7/000Base/os/root/dzinit.sh
@@ -7,7 +7,7 @@ chmod -R 755 /root/.dz
 
 fileList=(
   "/root/.dz.bashrc"
-  "/root/.dz/dzinit.sh"
+  "/root/.dz/dzfile.sh"
   "/root/.dz/dzcheck.sh"
   "/root/.dz/dzsys.sh"
   "/root/.dz/dzset.sh"
@@ -16,8 +16,8 @@ fileList=(
 for file in ${fileList[@]}; do
   echo ""
   echo "   Deleting: " $file
-  rm $file
+  rm -f $file
   echo "Downloading: " $file
   echo ""
-  curl -o $file -fsSL $pathRoot$file
+  curl -o $file -fsSL $pathRoot/$file
 done
