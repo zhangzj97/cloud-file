9d38e2d02a5ff314cf83f61d1895e74fd91f1929
Dylan Zhang <987883389@qq.com>
2022-12-09 00:28:23 +0800

    feat: add dzcheck to check base info

---
+++ CentOS7/000Base/os/root/.dz/dzcheck.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+IPADDR=$(cat /etc/sysconfig/network-scripts/ifcfg-ens33 | grep IPADDR)
+hostname=$(hostnamectl | grep 'Static hostname')
+
+if [ ! $IPADDR ]; then
+  echo ""
+  echo "[Error]: IPADDR is none"
+  echo ""
+  exit;
+fi
+
+echo ""
+echo $hostname
+echo ""
+echo $IPADDR
+echo ""
