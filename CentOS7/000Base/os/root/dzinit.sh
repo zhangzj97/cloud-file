3c8912256e30f7000addaf38f9c9907302cb0dda
Dylan Zhang <987883389@qq.com>
2022-12-09 03:09:57 +0800

    feat: fileList

--- CentOS7/000Base/os/root/dzinit.sh
+++ CentOS7/000Base/os/root/dzinit.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-pathRoot=https://raw.githubusercontent.com/zhangzj97/cloud-file/main/CentOS7/000Base/os/root
+pathRoot=https://raw.githubusercontent.com/zhangzj97/cloud-file/main/CentOS7/000Base/os/
 
-mkdir /root/.dz
+mkdir -p /root/.dz
 chmod -R 755 /root/.dz
 
-curl -o /root/.dz.bashrc     -fsSL $pathRoot/.dz.bashrc
-curl -o /root/.dz/dzinit.sh  -fsSL $pathRoot/.dz/dzinit.sh
-curl -o /root/.dz/dzcheck.sh -fsSL $pathRoot/.dz/dzcheck.sh
-curl -o /root/.dz/dzsys.sh   -fsSL $pathRoot/.dz/dzsys.sh
-curl -o /root/.dz/dzset.sh   -fsSL $pathRoot/.dz/dzset.sh
+fileList=(
+  "/root/.dz.bashrc",
+  "/root/.dz/dzinit.sh",
+  "/root/.dz/dzcheck.sh",
+  "/root/.dz/dzsys.sh",
+  "/root/.dz/dzset.sh",
+)
+
+for file in ${fileList[@]}; do
+  rm $file
+  curl -o $file -fsSL $pathRoot$file
+done
