5be9dc4a538f960a46cb389390def6ef80100aa3
Dylan Zhang <987883389@qq.com>
2022-12-09 12:39:59 +0800

    feat: dzfile

--- CentOS7/000Base/os/root/.dz/dzfile.sh
+++ CentOS7/000Base/os/root/.dz/dzfile.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-remoteUrl=https://raw.githubusercontent.com/zhangzj97/cloud-file/blob/main/CentOS7/000Base/os/root
-osName=CentOS7
+pathRoot=https://raw.githubusercontent.com/zhangzj97/cloud-file/blob/main/CentOS7/000Base/os/
 
-pathRoot=$remoteUrl/$onName/000Base/os/root
+fileList=(
+    "/root/.dz/dzalias.sh"
+    "/root/.dz/dzcheck.sh"
+    "/root/.dz/dzsys.sh"
+    "/root/.dz/dzset.sh"
+)
 
-curl -o /root/.dz.bashrc     -fsSL $pathRoot/.dz.bashrc
-curl -o /root/.dz/dzinit.sh  -fsSL $pathRoot/.dz/dzinit.sh
-curl -o /root/.dz/dzcheck.sh -fsSL $pathRoot/.dz/dzcheck.sh
-curl -o /root/.dz/dzsys.sh   -fsSL $pathRoot/.dz/dzsys.sh
-curl -o /root/.dz/dzset.sh   -fsSL $pathRoot/.dz/dzset.sh
+for file in ${fileList[@]}; do
+    echo ""
+    echo "   Deleting: " $file
+    rm -f $file
+    echo "Downloading: " $file
+    echo "     Source: " $pathRoot/$file
+    echo ""
+    curl -o $file -fsSL $pathRoot/$file
+done
--- CentOS7/000Base/os/root/dzinit.sh
+++ CentOS7/000Base/os/root/dzinit.sh
@@ -3,30 +3,23 @@
 ipAddrNew=$1
 hostNameNew=$2
 
-pathRoot=https://raw.githubusercontent.com/zhangzj97/cloud-file/main/CentOS7/000Base/os/
+sourceRemote=https://raw.githubusercontent.com/zhangzj97/cloud-file/main/CentOS7/000Base/os/
+file=/root/.dz/dzfile.sh
 
 mkdir -p /root/.dz
 chmod -R 755 /root/.dz
 
-fileList=(
-  "/root/.dz/dzalias.sh"
-  "/root/.dz/dzfile.sh"
-  "/root/.dz/dzcheck.sh"
-  "/root/.dz/dzsys.sh"
-  "/root/.dz/dzset.sh"
-)
-
-for file in ${fileList[@]}; do
-  echo ""
-  echo "   Deleting: " $file
-  rm -f $file
-  echo "Downloading: " $file
-  echo "     Source: " $pathRoot/$file
-  echo ""
-  curl -o $file -fsSL $pathRoot/$file
-done
+echo ""
+echo "   Deleting: " $file
+rm -f $file
+echo "Downloading: " $file
+echo "     Source: " $sourceRemote/$file
+echo ""
+curl -o $file -fsSL $sourceRemote/$file
 
 chmod -R 755 /root/.dz
+
+/root/.dz/dzfile.sh
 
 /root/.dz/dzalias.sh
 
