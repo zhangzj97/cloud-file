c38b3ef070754b92cb332a4d9becbf0eeb9d60bf
Dylan Zhang <987883389@qq.com>
2022-12-09 12:07:28 +0800

    chore: rename

--- CentOS7/000Base/os/root/.dz.bashrc
+++
@@ -1,8 +0,0 @@
-# .bashrc
-
-# User specific aliases and functions
-
-alias dzcheck="/root/.dz/dzcheck.sh"  
-alias dzset="/root/.dz/dzset.sh"
-alias dzsys="/root/.dz/dzsys.sh"
-alias dzinit="/root/.dz/dzinit.sh"
---
+++ CentOS7/000Base/os/root/.dz/dzalias.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo '# Start dz' >>/root/.bashrc
+echo 'alias dzcheck="/root/.dz/dzcheck.sh"' >>/root/.bashrc
+echo 'alias dzset="/root/.dz/dzset.sh"' >>/root/.bashrc
+echo 'alias dzsys="/root/.dz/dzsys.sh"' >>/root/.bashrc
+echo 'alias dzinit="/root/.dz/dzinit.sh"' >>/root/.bashrc
+echo '# End dz' >>/root/.bashrc
+echo '' >>/root/.bashrc
+
+source /root/.bashrc
