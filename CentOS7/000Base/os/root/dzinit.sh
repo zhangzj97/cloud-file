7a75a1ef531666227d4621c7f04214fb36fb6b2a
Dylan Zhang <987883389@qq.com>
2022-12-09 15:00:59 +0800

    chore

--- CentOS7/000Base/os/root/dzinit.sh
+++ CentOS7/000Base/os/root/dzinit.sh
@@ -1,8 +1,5 @@
 #!/bin/bash -i
 
-ipAddrNew=$1
-hostNameNew=$2
-
 sourceRemote=https://raw.githubusercontent.com/zhangzj97/cloud-file/main/CentOS7/000Base/os/
 file=/root/.dz/dzfile.sh
 
@@ -22,5 +19,3 @@ chmod -R 755 /root/.dz
 /root/.dz/dzfile.sh
 
 /root/.dz/dzalias.sh
-
-/root/.dz/dzset.sh $ipAddrNew $hostNameNew
