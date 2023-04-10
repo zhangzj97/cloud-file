8329512e20998d2b40ba5a3c3c10bf75c53a3da8
Dylan Zhang <987883389@qq.com>
2023-04-10 22:41:46 +0800

    chore

--- CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-alias.sh
+++ CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-alias.sh
@@ -5,18 +5,18 @@
 sed -i '/# <Dz> Alias/,/# <\/Dz> Alias/d' /etc/hosts
 ## 添加 Alias
 echo '# <Dz> Alias' >>/root/.bashrc
-echo 'alias dz=/tmp/dz-clound-cli/src/dz-dispatch.sh' >>/root/.bashrc
-echo 'alias dzalias="/tmp/dz-clound-cli/src/dz-alias.sh"' >>/root/.bashrc
-echo 'alias dzhelp="/tmp/dz-clound-cli/src/dz-help.sh"' >>/root/.bashrc
-echo 'alias dzcheck="/tmp/dz-clound-cli/src/dz-check.sh"' >>/root/.bashrc
-echo 'alias dzfile="/tmp/dz-clound-cli/src/dz-file.sh"' >>/root/.bashrc
-echo 'alias dzfirewall="/tmp/dz-clound-cli/src/dz-firewall.sh"' >>/root/.bashrc
-echo 'alias dzhost="/tmp/dz-clound-cli/src/dz-host.sh"' >>/root/.bashrc
-echo 'alias dzreinit="/tmp/dz-clound-cli/src/dz-reinit.sh"' >>/root/.bashrc
-echo 'alias dzset="/tmp/dz-clound-cli/src/dz-set.sh"' >>/root/.bashrc
-echo 'alias dzssl="/tmp/dz-clound-cli/src/dz-ssl.sh"' >>/root/.bashrc
-echo 'alias dzsys="/tmp/dz-clound-cli/src/dz-sys.sh"' >>/root/.bashrc
-echo 'alias dzyum="/tmp/dz-clound-cli/src/dz-yum.sh"' >>/root/.bashrc
+echo 'alias dz=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-dispatch.sh' >>/root/.bashrc
+echo 'alias dzalias=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-alias.sh' >>/root/.bashrc
+echo 'alias dzhelp=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-help.sh' >>/root/.bashrc
+echo 'alias dzcheck=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-check.sh' >>/root/.bashrc
+echo 'alias dzfile=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-file.sh' >>/root/.bashrc
+echo 'alias dzfirewall=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-firewall.sh' >>/root/.bashrc
+echo 'alias dzhost=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-host.sh' >>/root/.bashrc
+echo 'alias dzreinit=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-reinit.sh' >>/root/.bashrc
+echo 'alias dzset=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-set.sh' >>/root/.bashrc
+echo 'alias dzssl=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-ssl.sh' >>/root/.bashrc
+echo 'alias dzsys=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-sys.sh' >>/root/.bashrc
+echo 'alias dzyum=/tmp/cloud-file/CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-yum.sh' >>/root/.bashrc
 echo '# </Dz> Alias' >>/root/.bashrc
 
 source /root/.bashrc
