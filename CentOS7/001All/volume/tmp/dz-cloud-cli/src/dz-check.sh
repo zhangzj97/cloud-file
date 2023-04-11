3ba3b1e345605a9c12a68bcf41d18e2e5314fe94
Dylan Zhang <987883389@qq.com>
2023-04-11 20:49:11 +0800

    chore

--- CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-check.sh
+++ CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-check.sh
@@ -4,14 +4,8 @@ IPADDR=$(cat /etc/sysconfig/network-scripts/ifcfg-ens33 | grep IPADDR)
 hostname=$(hostnamectl | grep 'Static hostname')
 
 if [ ! $IPADDR ]; then
-  echo ""
-  echo "[Error]: IPADDR is none"
-  echo ""
-  exit
+  echo "[Error]: IPADDR is none" && exit
 fi
 
-echo ""
 echo $hostname
-echo ""
 echo $IPADDR
-echo ""
