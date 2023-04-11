e128e5e02a2d5c62782864846b08f90e95f9eff8
Dylan Zhang <987883389@qq.com>
2023-04-11 17:56:18 +0800

    chore

--- CentOS7/001All/install.sh
+++ CentOS7/001All/install.sh
@@ -51,31 +51,38 @@ wget -t 20 -O /tmp/cloud-file.tar.gz https://github.com/zhangzj97/cloud-file/arc
 ### TODO 存在异步问题
 sleep 10
 tar -xvf /tmp/cloud-file.tar.gz
-rm -fr /tmp/cloud-file
-mv /tmp/cloud-file-$CloudFileVersion /tmp/cloud-file
-# rm -f /tmp/cloud-file.tar.gz
-
-# Test
-## 从 github 下载一个发行版本
-wget -t 20 -O /tmp/cloud-file.tar.gz https://github.com/xxxxx.tar.gz
-## 解压
-tar -zxvf /tmp/cloud-file.tar.gz
-
-### 加一个 目录存在的测试
 
 if [[ ! -x /tmp/cloud-file-$CloudFileVersion ]]; then
     echo /tmp/cloud-file-$CloudFileVersion "No Exist"
 fi
 
-## /tmp/cloud-file-1.1.1 => /tmp/cloud-file 去除文件夹上的版本号
-### !!! 理论上 会有一个 /tmp/cloud-file-1.1.1 被解压出来
-### !!! 但是 mv 报错 no such file or ...
-mv /tmp/cloud-file-$Version /tmp/cloud-file
+rm -fr /tmp/cloud-file
+mv /tmp/cloud-file-$CloudFileVersion /tmp/cloud-file
+# rm -f /tmp/cloud-file.tar.gz
 
-# 情况
-## 如果 控制台直接执行 是可以的
-## 但如果 是 bash xxx.sh 就会报错
+###########
 
-# 问
-## 1. 是不是和异步相关 , tar 是异步的
-### 实验了一下， tar 后 ， 目标文件夹（/tmp/cloud-file-1.1.1）不存在
+# # Test
+# ## 从 github 下载一个发行版本
+# wget -t 20 -O /tmp/cloud-file.tar.gz https://github.com/xxxxx.tar.gz
+# ## 解压
+# tar -zxvf /tmp/cloud-file.tar.gz
+
+# ### 加一个 目录存在的测试
+
+# if [[ ! -x /tmp/cloud-file-$CloudFileVersion ]]; then
+#     echo /tmp/cloud-file-$CloudFileVersion "No Exist"
+# fi
+
+# ## /tmp/cloud-file-1.1.1 => /tmp/cloud-file 去除文件夹上的版本号
+# ### !!! 理论上 会有一个 /tmp/cloud-file-1.1.1 被解压出来
+# ### !!! 但是 mv 报错 no such file or ...
+# mv /tmp/cloud-file-$Version /tmp/cloud-file
+
+# # 情况
+# ## 如果 控制台直接执行 是可以的
+# ## 但如果 是 bash xxx.sh 就会报错
+
+# # 问
+# ## 1. 是不是和异步相关 , tar 是异步的
+# ### 实验了一下， tar 后 ， 目标文件夹（/tmp/cloud-file-1.1.1）不存在
