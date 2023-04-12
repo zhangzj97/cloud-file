0b76aea4503721c745999127c20fd1f0120151b7
Dylan Zhang <987883389@qq.com>
2023-04-12 22:40:25 +0800

    chore

--- CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-k8s.sh
+++ CentOS7/001All/volume/tmp/dz-cloud-cli/src/dz-k8s.sh
@@ -70,3 +70,92 @@ echo 'modprobe -- nf_conntrack_ipv4' >>/etc/sysconfig/modules/ipvs.modules
 chmod +x /etc/sysconfig/modules/ipvs.modules
 source /etc/sysconfig/modules/ipvs.modules
 lsmod | grep -e ip_vs -e nf_conntrack_ipv4
+
+# 安转 K8S
+## 复制 repo 文件
+mv /tmp/cloud-file/CentOS7/001All/volume/etc/yum.repos.d/dz-kubernetes.repo /etc/yum.repos.d/docker-ce.repo
+
+# cat <<EOF >/etc/yum.repos.d/dz-kubernetes.repo
+# [kubernetes]
+# name=Kubernetes
+# baseurl=http://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
+# enabled=1
+# gpgcheck=0
+# EOF
+# cat /etc/yum.repos.d/dz-kubernetes.repo
+
+## 安装 组件
+yum remove -y kubelet kubeadm kubectl
+yum install -y --setopt=obsoletes=0 kubelet-1.17.4-0 kubectl-1.17.4-0 kubeadm-1.17.4-0
+
+## 配置
+if [ ! -f /etc/sysconfig/kubelet ]; then
+  touch /etc/sysconfig/kubelet
+fi
+sed -i '/# <Dz> K8S/,/# <\/Dz> K8S/d' /etc/sysconfig/kubelet
+echo '# <Dz> K8S ' >>/etc/sysconfig/kubelet
+echo 'KUBELET_CGROUP_ARGS=--cgroup-driver=systemd' >>/etc/sysconfig/kubelet
+echo 'KUBE_PROXY_MODE=ipvs' >>/etc/sysconfig/kubelet
+echo '# </Dz> K8S' >>/etc/sysconfig/kubelet
+
+## 开启 kubelet
+systemctl start kubelet
+systemctl enable kubelet
+
+## 准备集群镜像
+kubeadm config images list
+images=(
+  kube-apiserver:v1.17.4
+  kube-controller-manager:v1.17.4
+  kube-scheduler:v1.17.4
+  kube-proxy:v1.17.4
+  pause:3.1
+  etcd:3.4.3-0
+  coredns:1.6.5
+)
+for imageName in ${images[@]}; do
+  docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
+  docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
+  docker rmi registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
+done
+docker images
+## 初始化集群
+## TODO 学习 参数意义
+# kubeadm init --kubernetes-version=v1.17.4 --apiserver-advertise-address=192.168.226.201 --pod-network-cidr=10.244.0.0/16 --service-cidr=10.96.0.0/12
+kubeadm init \
+  --kubernetes-version=v1.17.4 \
+  --apiserver-advertise-address=192.168.226.201 \
+  --pod-network-cidr=10.244.0.0/16 \
+  --service-cidr=10.96.0.0/12
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+## kuebadm toke
+## kubeadm init 会生成 token 以供 worker 去 kubeadm join
+kubeadm token list
+kubeadm token create --print-join-command
+## 永久token
+# kubeadm token create --ttl 0 --print-join-command
+
+# 安装 docker
+# wget https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo -O /etc/yum.repos.d/docker.repo
+
+## 复制 repo 文件
+mv /tmp/cloud-file/CentOS7/001All/volume/etc/yum.repos.d/dz-docker.repo /etc/yum.repos.d/dz-docker.repo
+
+## 安装 docker-ce
+yum install -y --setopt=obsoletes=0 docker-ce-18.06.3.ce-3.el7
+
+## 配置
+## Docker在默认情况下使用的Cgroup Driver为cgroupfs，而kubernetes推荐使用systemd来代替cgroupfs
+rm -f /etc/docker/daemon.json
+mkdir /etc/docker
+touch /etc/docker/daemon.json
+echo '{' >>/etc/docker/daemon.json
+echo '"exec-opts": ["native.cgroupdriver=systemd"],' >>/etc/docker/daemon.json
+echo '"registry-mirrors": ["https://kn0t2bca.mirror.aliyuncs.com"]' >>/etc/docker/daemon.json
+echo '}' >>/etc/docker/daemon.json
+
+## 开启 docker
+systemctl restart docker
+systemctl enable docker
