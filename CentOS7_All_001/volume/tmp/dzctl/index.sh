68c1b417ac74987f8258d8586262d2d179148855
Dylan Zhang <987883389@qq.com>
2023-04-18 16:29:46 +0800

    chore

---
+++ CentOS7_All_001/volume/tmp/dzctl/index.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -i
+
+PluginPath=./src/plugins
+
+ComponentCode=$1
+ComponentActionCode=$2
+echo $1 $2 $PluginPath
+if [[ $ComponentCode =~ 'host' ]]; then
+    source $PluginPath/dz-host/index.sh
+fi
