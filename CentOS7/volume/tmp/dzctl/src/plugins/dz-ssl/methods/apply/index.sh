681a5eee740d5e155d430a6f908bb0b49977b4ed
Dylan Zhang <987883389@qq.com>
2023-04-23 00:35:28 +0800

    chore

--- CentOS7/volume/tmp/dzctl/src/plugins/dz-ssl/methods/apply/index.sh
+++ CentOS7/volume/tmp/dzctl/src/plugins/dz-ssl/methods/apply/index.sh
@@ -1,75 +1,46 @@
 #!/bin/bash -i
 
-RED='\e[1;31m'    # 红
-GREEN='\e[1;32m'  # 绿
-YELLOW='\e[1;33m' # 黄
-BLUE='\e[1;34m'   # 蓝
-PINK='\e[1;35m'   # 粉红
-RES='\e[0m'       # 清除颜色
+source $DZ_TOOL_PATH
 
-logStage() {
-  echo ""
-  echo ""
-  echo -e "${BLUE}                ============================================================"
-  echo -e "${BLUE}                $1 ${RES}"
-}
+StageNo=0
 
-logStep() {
-  echo -e "                    $1"
-}
-
-logResult() {
-  echo -e "${GREEN}                                [RESULT] Finish Stage Successfully! ${RES}"
-  echo ""
-}
-
-logErrorResult() {
-  echo -e "${RED}                    [Error] $1 ${RES}"
-  echo ""
-}
-
-cpDir() {
-  rm -fr $2
-  mkdir -p $2
-  /bin/cp -fa $1/* $2
-}
-
-StageRemark=(
-  "[Stage00]"
-  "[Stage01] Validate          | Validate param"
-)
-
-# Validate | Validate param
-logStage "${StageRemark[1]}"
-# [[ $* =~ get ]] && echo Error apply && exit
-
-# Certificate | Generate Certificate
-logStage "${StageRemark[2]}"
+logStage $StageNo "Check Certificate Authority"
+$ServerDomainPort=$1
+[[ ! $ServerDomainPort ]] && echo "Need doamin and port" && exit 0
 DzCertsdPath=/etc/dz/certs.d
-TargetCertsdPath=$DzCertsdPath/localhost
-# Generate a Certificate Authority Certificate
-logStep "[Certificate] Config path"
-mkdir -p $DzCertsdPath
-mkdir -p $TargetCertsdPath
+ServerCertsdPath=$DzCertsdPath/$ServerDomainPort
 CaKeyPath=$DzCertsdPath/ca.key
 CaCrtPath=$DzCertsdPath/ca.crt
-logStep "[Certificate] Generate a Certificate Authority Certificate"
-openssl genrsa -out $CaKeyPath 4096
-openssl req -x509 -new -nodes -sha512 -days 3650 -subj "/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=zhangzejie.top" -key $CaKeyPath -out $CaCrtPath
-# Generate a Generate a Server Certificate
-logStep "[Certificate] Generate a Generate a Server Certificate"
-TargetKeyPath=$TargetCertsdPath/localhost.key
-TargetCsrPath=$TargetCertsdPath/localhost.csr
-TargetCrtPath=$TargetCertsdPath/localhost.crt
-TargetCertPath=$TargetCertsdPath/localhost.cert
-openssl genrsa -out $TargetKeyPath 4096
-openssl req -sha512 -new -subj "/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=192.168.226.100" -key $TargetKeyPath -out $TargetCsrPath
+if [[ ! -f $CaKeyPath ]]; then
+  logStep "[Certificate] Generate CA Certificate"
+  logStep "${Space04}===> ${DzCertsdPath}"
+  logStep "${Space04}===> ${ServerCertsdPath}"
+  mkdir -p $DzCertsdPath
+  mkdir -p $ServerCertsdPath
+  openssl genrsa -out $CaKeyPath 4096
+  CaSubj="/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=zhangzejie.top"
+  openssl req -x509 -new -nodes -sha512 -days 3650 -subj $CaSubj -key $CaKeyPath -out $CaCrtPath
+fi
+let StageNo+=1
+
+logStage $StageNo "Generate a Server Certificate"
+ServerKeyPath=$ServerCertsdPath/Server.key
+ServerCsrPath=$ServerCertsdPath/Server.csr
+ServerCrtPath=$ServerCertsdPath/Server.crt
+ServerCertPath=$ServerCertsdPath/Server.cert
+[[ -f $ServerKeyPath ]] && echo "Doamin exists" && exit 0
+logStep "[Certificate] Generate Server key"
+openssl genrsa -out $ServerKeyPath 4096
+ServerSubj="/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=zhangzejie.top"
+openssl req -sha512 -new -subj $ServerSubj -key $ServerKeyPath -out $ServerCsrPath
 # Generate an x509 v3 extension file
 logStep "[Certificate] Generate an x509 v3 extension file"
-V3ExtPath=$TargetCertsdPath/v3.ext
+V3ExtPath=$ServerCertsdPath/v3.ext
 /bin/cp -fa $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/certs.d/v3.ext $V3ExtPath
-openssl x509 -req -sha512 -days 3650 -extfile $V3ExtPath -CA $CaCrtPath -CAkey $CaKeyPath -CAcreateserial -in $TargetCsrPath -out $TargetCrtPath
-openssl x509 -inform PEM -in $TargetCrtPath -out $TargetCertPath
-# Copy dir
-cpDir $TargetCertsdPath /etc/docker/certs.d/localhost
+openssl x509 -req -sha512 -days 3650 -extfile $V3ExtPath -CA $CaCrtPath -CAkey $CaKeyPath -CAcreateserial -in $ServerCsrPath -out $ServerCrtPath
+openssl x509 -inform PEM -in $ServerCrtPath -out $ServerCertPath
+logStep "[Certificate] Copy file"
+logStep "${Space04}===> /etc/docker/certs.d/${ServerDomainPort}"
+cpDir $DzCertsdPath/$ServerDomainPort /etc/docker/certs.d/$ServerDomainPort
+
 systemctl restart docker
