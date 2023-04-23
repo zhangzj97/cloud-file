630a54aac1a41f0bc5ef9a1c236e88aef28b0344
Dylan Zhang <987883389@qq.com>
2023-04-23 21:30:26 +0800

    chore

--- CentOS7/volume/tmp/dzctl/src/plugins/dz-ssl/methods/apply/index.sh
+++ CentOS7/volume/tmp/dzctl/src/plugins/dz-ssl/methods/apply/index.sh
@@ -34,11 +34,10 @@ CaKeyPath=$DzCertsdPath/ca.key
 CaCrtPath=$DzCertsdPath/ca.crt
 if [[ ! -f $CaKeyPath ]]; then
   logStep "[Certificate] Generate CA Certificate"
-  logStep "${Space04}==> ${DzCertsdPath}"
-  mkdir -p $DzCertsdPath
-  openssl genrsa -out $CaKeyPath 4096
+  logDir $DzCertsdPath && mkdir -p $DzCertsdPath
   CaSubj="/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=zhangzejie.top"
-  openssl req -x509 -new -nodes -sha512 -days 3650 -subj $CaSubj -key $CaKeyPath -out $CaCrtPath
+  logFile $CaKeyPath && openssl genrsa -out $CaKeyPath 4096
+  logFile $CaCrtPath && openssl req -x509 -new -nodes -sha512 -days 3650 -subj $CaSubj -key $CaKeyPath -out $CaCrtPath
 fi
 let StageNo+=1
 
@@ -49,20 +48,21 @@ ServerCrtPath=$ServerCertsdPath/server.crt
 ServerCertPath=$ServerCertsdPath/server.cert
 [[ -f $ServerKeyPath ]] && logErrorResult "Doamin exists" && exit 0
 logStep "[Certificate] Generate Server key"
-logStep "${Space04}==> ${ServerCertsdPath}"
-mkdir -p $ServerCertsdPath
+logDir $ServerCertsdPath && mkdir -p $ServerCertsdPath
 openssl genrsa -out $ServerKeyPath 4096
 ServerSubj="/C=CN/ST=Beijing/L=Beijing/O=example/OU=Personal/CN=zhangzejie.top"
 openssl req -sha512 -new -subj $ServerSubj -key $ServerKeyPath -out $ServerCsrPath
 # Generate an x509 v3 extension file
 logStep "[Certificate] Generate an x509 v3 extension file"
 V3ExtPath=$ServerCertsdPath/v3.ext
-/bin/cp -fa $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/certs.d/v3.ext $V3ExtPath
-openssl x509 -req -sha512 -days 3650 -extfile $V3ExtPath -CA $CaCrtPath -CAkey $CaKeyPath -CAcreateserial -in $ServerCsrPath -out $ServerCrtPath
-openssl x509 -inform PEM -in $ServerCrtPath -out $ServerCertPath
-logStep "[Certificate] Copy file"
-logStep "${Space04}==> /etc/docker/certs.d/${ServerDomainPort}"
-cpDir $DzCertsdPath/$ServerDomainPort /etc/docker/certs.d/$ServerDomainPort
+logFile $V3ExtPath &&
+  /bin/cp -fa $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/certs.d/v3.ext $V3ExtPath
+logFile $ServerCrtPath &&
+  openssl x509 -req -sha512 -days 3650 -extfile $V3ExtPath -CA $CaCrtPath -CAkey $CaKeyPath -CAcreateserial -in $ServerCsrPath -out $ServerCrtPath
+logFile $ServerCertPath &&
+  openssl x509 -inform PEM -in $ServerCrtPath -out $ServerCertPath
+logDir /etc/docker/certs.d/$ServerDomainPort &&
+  cpDir $DzCertsdPath/$ServerDomainPort /etc/docker/certs.d/$ServerDomainPort
 let StageNo+=1
 
 logStage $StageNo "Restart Service"
