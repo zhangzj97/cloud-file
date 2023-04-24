94e58f95a703dc271aa1ec7815d78fcbbc742d7a
Dylan Zhang <987883389@qq.com>
2023-04-24 13:58:00 +0800

    chore

--- CentOS7/volume/tmp/dzctl/src/plugins/dz-docker/methods/dashboard/index.sh
+++ CentOS7/volume/tmp/dzctl/src/plugins/dz-docker/methods/dashboard/index.sh
@@ -27,11 +27,13 @@ StageNo=0
 if [[ $WebMode ]]; then
   logStage $StageNo "Deploy Docker Dashboard WebMode"
   docker pull joinsunsoft/docker.ui &&
-    docker tag joinsunsoft/docker.ui dz-dashboard-docker.ui:1.0.0
+    docker tag joinsunsoft/docker.ui dz-dashboard-docker-ui:1.0.0 # !!! 不是 docker.ui
   docker pull portainer/portainer-ce &&
     docker tag portainer/portainer-ce dz-dashboard-portainer-ce:1.0.0
-  docker compose -f $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/docker-compose/dz-dashboard/docker-compose.yml up -d &&
-    logFile $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/docker-compose/dz-dashboard/docker-compose.yml
+  docker compose -f $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/docker-compose/dz-dashboard/docker-compose.yml up -d &&
+    logStep "192.168.226.100:9001 ==> " &&
+    logStep "192.168.226.100:9002 ==> " &&
+    logFile $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/docker-compose/dz-dashboard/docker-compose.yml
   let StageNo+=1
 fi
 
