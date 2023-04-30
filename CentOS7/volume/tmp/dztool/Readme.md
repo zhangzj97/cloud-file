b4067f896daaaf17eab385843452a17afbd28006
Dylan Zhang <987883389@qq.com>
2023-04-30 20:56:46 +0800

    chore

---
+++ CentOS7/volume/tmp/dztool/Readme.md
@@ -0,0 +1,113 @@
+# DzTool
+
+## 日志模块 dz-log
+
+- dzLogStage $StageNo $Description
+- dzLogInfo $Description
+- dzLogWarning $Description
+- dzLogError $Description
+
+```bash
+# 日志 - 阶段
+# dzLogStage $StageNo $Description
+dzLogStage () {}
+
+# 日志 - Level Info
+# dzLogInfo $Description
+dzLogInfo () {}
+
+# 日志 - Level Warning
+# dzLogWarning $Description
+dzLogWarning () {}
+
+# 日志 - Level Error
+# dzLogError $Description
+dzLogError () {}
+
+```
+
+## 文件模块 dz-fs 建议只在终端使用
+
+- dzFsEdit $FilePath $Sed
+- dzFsMatch $FilePath $Sed
+- dzFsRm $FilePath
+- dzFsBak $FilePath
+- dzFsDir $FilePath
+
+## 备份空间文件模块 dz-bakfs
+
+- dzBakFsPush $FilePath
+- dzBakFsClear
+
+```bash
+# 备份空间 备份
+# dzBakFsPush $FilePath
+dzBakFsPush () {}
+
+# 备份空间 清除
+# dzBakFsClear
+dzBakFsClear () {}
+
+```
+
+## 临时空间文件模块 dz-tmpfs
+
+- dzTmpFsPush $FilePath $RemoteSource
+- dzTmpFsPush $FilePath $Source
+- dzTmpFsPush $FilePath $Source/TmpFsKeep
+- dzTmpFsPush $FilePath
+- dzTmpFsEdit $FilePath $Sed
+- dzTmpFsMatch $FilePath $Sed
+- dzTmpFsPull $FilePath
+- dzTmpFsPull $FilePath TmpFsRemove
+- dzTmpFsPull $FilePath TmpFsBackup
+
+```bash
+# 在 TmpFs 添加内容
+# dzTmpFsPush $FilePath
+# dzTmpFsPush $FilePath $RemoteSource
+# dzTmpFsPush $FilePath $Source
+# dzTmpFsPush $FilePath $Source/TmpFsKeep
+dzTmpFsPush () {}
+
+# 编辑文件内容
+# dzTmpFsEdit $FilePath $Sed
+dzTmpFsEdit () {}
+
+# 正则匹配内容获取字段
+# dzTmpFsMatch $FilePath $Sed
+dzTmpFsMatch () {}
+
+# 从 TmpFs 获取内容
+# dzTmpFsPull $FilePath
+# dzTmpFsPull $FilePath TmpFsRemove
+# dzTmpFsPull $FilePath TmpFsBackup
+dzTmpFsPull () {}
+
+```
+
+## 未分类模块 dz-other
+
+- dzTarC $FilePath $Source
+- dzTarX $FilePath $Target
+- dzRpmYum $RpmName $CheckName
+- dzLinkFile $FilePath $BinName
+
+```bash
+# Tar 压缩
+# dzTarC $FilePath $Source
+dzTarC () {}
+
+# Tar 解压
+# dzTarX $FilePath $Target
+dzTarX () {}
+
+# Rpm
+# dzRpmYum $RpmName $CheckName
+dzRpmYum () {}
+
+# 关联
+# dzLinkFile $FilePath $BinName
+dzLinkFile () {}
+
+```
--- CentOS7/volume/tmp/dztool/index.sh
+++ CentOS7/volume/tmp/dztool/index.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -i
 
+###################################################################################################
+## 日志模块 dz-log
+###################################################################################################
+
 TextRed='\e[1;31m'
 TextGreen='\e[1;32m'
 TextYellow='\e[1;33m'
@@ -16,79 +20,202 @@ Space24=$Space20$Space08
 Space28=$Space24$Space04
 Space32=$Space28$Space04
 
-logStage() {
-  echo ""
+# 日志 - 阶段
+# dzLogStage $StageNo $Description
+dzLogStage() {
+  StageNo=$1
+  Description=$2
+
   echo ""
   echo -e "${Space16}${TextBlue}============================================================"
-  echo -e "${Space16}${TextBlue}[Stage$1] $2${TextClear}"
+  echo -e "${Space16}${TextBlue}[Stage$StageNo] $Description${TextClear}"
 }
 
-logStep() {
-  echo -e "${Space16}$1"
-}
+# 日志 - Level Info
+# dzLogInfo $Description
+dzLogInfo() {
+  Description=$1
 
-# logValue Label CurrentValue PrevValue
-logValue() {
-  echo -e "${Space16}[$1] $3 => ${TextBlue}$2${TextClear}"
-}
-
-# logFile file
-logFile() {
-  echo -e "${Space16}${TextPink}[Relative] $1${TextClear}"
-}
-
-# logDir dir
-logDir() {
-  echo -e "${Space16}${TextPink}[Relative] $1${TextClear}"
-}
-
-logErrorResult() {
-  echo -e "${Space16}${TextRed}[Error] $1${TextClear}"
+  echo -e "${Space16}[INFO   ] $Description"
   echo ""
 }
 
-# dzYum source target
-cpDir() {
-  rm -fr $2
-  mkdir -p $2
-  /bin/cp -fa $1/* $2
+# 日志 - Level Warning
+# dzLogWarning $Description
+dzLogWarning() {
+  Description=$1
+
+  echo -e "${Space16}${TextYellow}[WARNING] $Description${TextClear}"
+  echo ""
 }
 
-# dzYum file bin
-lnSh() {
-  chmod u+x $1
-  ln -fs $1 /bin/$2
+# 日志 - Level Error
+# dzLogError $Description
+dzLogError() {
+  Description=$1
+
+  echo -e "${Space16}${TextRed}[ERROR  ] $Description${TextClear}"
+  echo ""
 }
 
-# dzYum rpm
-dzYum() {
-  if [[ ! $(rpm -qa | grep $1) ]]; then
-    yum install -y -q $1
+###################################################################################################
+## 临时空间文件模块 dz-tmp-fs
+###################################################################################################
+
+# 在 TmpFs 添加内容
+# dzTmpFsPush $FilePath
+# dzTmpFsPush $FilePath $RemoteSource
+# dzTmpFsPush $FilePath $Source
+# dzTmpFsPush $FilePath $Source/TmpFsKeep
+dzTmpFsPush() {
+  DzTmpFsPath=$DZ_TMP_FS_PATH
+  TmpFsRemove="TmpFsRemove"
+  TmpFsRemove="TmpFsRemove"
+
+  # 验证 FilePath
+  # 1. 有
+  # 2. 不在根目录下
+  # 3. 有文件名称
+  FilePath=$1
+  FilePathDir=${FilePath%/*}
+  FilePathFileName=${FilePath##*/}
+  DzTmpFsFilePath=$DzTmpFsPath/$FilePath
+  DzTmpFsFilePathDir=$DzTmpFsPath/$FilePathDir
+
+  [[ ! $FilePath ]] && dzLogError "dzTmpFsPush => FilePath is required" && exit
+  [[ $FilePathDir = / ]] && dzLogError "dzTmpFsPush => FilePathDir is /, please change another" && exit
+  [[ ! $FilePathFileName ]] && dzLogError "dzTmpFsPush => FilePath : FilePathFileName is invalid" && exit
+
+  # 验证 Source
+  # 1. 如果没有, 默认 Source=FilePath
+  # 2. 如果 http 开头, 远程下载
+  # 3. 如果是本地文件
+  #   3.1 如果不存在 => 在 tmpfs 新建一个空文件
+  #   3.2 如果存在 => 复制到 tmpfs
+  Source=$2
+  RemoteFlag=
+  [[ ! $Source ]] && Source=$FilePath
+  [[ $Source =~ http ]] && RemoteFlag=1
+
+  # 重置 tmpfs 空间
+  rm -rf $FilePathFileName
+  mkdir -p $FilePathDir
+  mkdir -p $DzTmpFsFilePathDir
+
+  # 获取 Source
+  if [[ $RemoteFlag && ! $(rpm -qa | grep wget) ]]; then
+    curl -fsSL $Source >$DzTmpFsFilePath
+  elif [[ $RemoteFlag && $(rpm -qa | grep wget) ]]; then
+    wget -t0 -T5 -O $DzTmpFsFilePath $Source --no-check-certificate
+  elif [[ ! $RemoteFlag && ! -f $Source ]]; then
+    touch $DzTmpFsFilePath
+  elif [[ ! $RemoteFlag && -f $Source ]]; then
+    /bin/cp -fa $Source $DzTmpFsFilePath
   fi
 }
 
-# dzTarc file label
-dzTextRemove() {
-  sed -i "/# <Dz> $2/,/# <\/Dz> $2/d" $1
+# 编辑文件内容
+# dzTmpFsEdit $FilePath $Sed
+dzTmpFsEdit() {
+  FilePath=$1
+  Sed=$2
+
+  sed -r -i "$Sed" $FilePath
 }
 
-# dzTarc file text
-dzTextAppend() {
-  echo $2 >>$1
+# 正则匹配内容获取字段
+# dzTmpFsMatch $FilePath $Sed
+dzTmpFsMatch() {
+  FilePath=$1
+  Sed=$2
+
+  sed -rz -e "$Sed" $FilePath
 }
 
-# dzTarc file dir
-dzTarc() {
-  tar -czf $1 $2
+# 从 TmpFs 获取内容 并 记录与备份 (仅仅支持获取同名文件)
+# dzTmpFsPull $FilePath
+# dzTmpFsPull $FilePath $TmpFsCode
+dzTmpFsPull() {
+  DzTmpFsPath=$DZ_TMP_FS_PATH
+  DzBakFsPath=$DZ_BAK_FS_PATH
+  TmpFsRemove="TmpFsRemove"
+
+  # 验证 FilePath
+  # 临时空间和本地空间文件同名
+  # 1. 先判断是否有 TmpFsCode, 有则优先执行
+  # 2. 没有 TmpFsCode, 临时空间没有, 报错
+  # 3. 没有 TmpFsCode, 临时空间有
+  #   3.2 本地空间没有 直接获取
+  #   3.1 本地空间有 备份并替换
+  FilePath=$1
+  FilePathDir=${FilePath%/*}
+  FilePathFileName=${FilePath##*/}
+  DzTmpFsFilePath=$DzTmpFsPath/$FilePath
+  DzTmpFsFilePathDir=$DzTmpFsPath/$FilePathDir
+  TmpFsCode=$2
+
+  # 验证 TmpFsCode
+  # 1. 先判断是否有 TmpFsCode, 有则优先执行
+  if [[ $TmpFsCode = TmpFsRemove && ! -f $FilePath ]]; then
+    dzLogInfo "删除文件 => $FilePath, 不存在"
+    return
+  elif [[ $TmpFsCode = TmpFsRemove && -f $FilePath ]]; then
+    TimeFlag=$(date "+%Y-%m-%d%H:%M:%S")
+    /bin/cp -fa $FilePath $DzBakFsPath/$FilePath-$TimeFlag.bak
+    rm -rf $FilePath
+    dzLogInfo "删除文件 => $FilePath, 备份 Path => $DzBakFsPath/$FilePath-$TimeFlag.bak"
+    return
+  fi
+
+  # 验证 FilePath
+  # 临时空间和本地空间文件同名
+  # 1. 没有 TmpFsCode, 临时空间没有, 报错
+  # 2. 没有 TmpFsCode, 临时空间有
+  #   2.1 本地空间没有 直接获取
+  #   2.2 本地空间有 备份并替换
+
+  if [[ ! -f $DzTmpFsFilePath ]]; then
+    dzLogError "dzTmpFsPull => ${DzTmpFsFilePath} is not found" && exit
+  elif [[ -f $DzTmpFsFilePath && ! -f $FilePath ]]; then
+    /bin/cp -fa $DzTmpFsFilePath $FilePath
+    dzLogInfo "新增文件 => $FilePath"
+  elif [[ -f $DzTmpFsFilePath && -f $FilePath ]]; then
+    TimeFlag=$(date "+%Y-%m-%d%H:%M:%S")
+    /bin/cp -fa $FilePath $DzBakFsPath/$FilePath-$TimeFlag.bak
+    rm -rf $FilePath
+    /bin/cp -fa $DzTmpFsFilePath $FilePath
+    dzLogInfo "修改文件 => $FilePath, 备份 Path => $DzBakFsPath/$FilePath-$TimeFlag.bak"
+  fi
+
 }
 
-# dzTarc file dir
-dzTarx() {
-  mkdir -p $2
-  tar -xzf $1 -C $2
+###################################################################################################
+## 临时空间文件模块 dz-tmp-fs
+###################################################################################################
+
+# Tar 压缩
+# dzTarC $FilePath $Source
+dzTarC() {
+  FilePath=$1
+  Source=$2
+
+  tar -czvPf $FilePath $Dir
 }
 
-# dzTarc file url
-dzWget() {
-  wget -t0 -T5 -O $1 $2 --no-check-certificate
+# Tar 解压
+# dzTarX $FilePath $Target
+dzTarX() {
+  FilePath=$1
+  Target=$2
+
+  mkdir -p $Target
+  tar -xzf $FilePath -C $Target
 }
+
+# Rpm
+# dzRpmYum $RpmName $CheckName
+dzRpmYum() {}
+
+# 关联
+# dzLinkFile $FilePath $BinName
+dzLinkFile() {}
