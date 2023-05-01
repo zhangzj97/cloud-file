5977d9de998b52096d3f5170ff11820967212859
Dylan Zhang <987883389@qq.com>
2023-05-01 14:30:39 +0800

    chore

---
+++ CentOS7/volume/tmp/dzctl/src/plugins/dz-server/methods/docker-cli/index.sh
@@ -0,0 +1,36 @@
+#!/bin/bash -i
+
+source $DZ_TOOL_PATH
+
+ARGS=$(getopt -l xxx: -n 'dzctl' -- "$@")
+[ $? != 0 ] && echo Erro options && exit
+eval set -- "${ARGS}"
+while true; do
+  case $1 in
+  --)
+    break
+    ;;
+  *)
+    dzLogError "Internal error!" && exit
+    ;;
+  esac
+done
+
+###################################################################################################
+## 业务
+###################################################################################################
+
+StageNo=1
+
+logStage $StageNo "检查 Docker"
+DockerVersion=$(rpm -qa | grep docker-ce)
+[[ ! $DockerVersion ]] && dzLogError "dzctl docker-cli => need docker-ce" && exit
+let StageNo+=1
+
+logStage $StageNo "运行 Docker Dashboard Cli"
+DzDockerDashboardCliDC=/etc/dz/docker-compose/dz-docker-dashboard-cli/docker-compose.yml
+docker pull lazyteam/lazydocker &&
+  docker tag lazyteam/lazydocker dz-docker-dashboard-lazydocker:1.0.0
+docker rmi lazyteam/lazydocker
+docker compose -f $DzDockerDashboardCliDC run dz-docker-dashboard-lazydocker
+let StageNo+=1
--- CentOS7/volume/tmp/dzctl/src/plugins/dz-server/methods/docker/index.sh
+++ CentOS7/volume/tmp/dzctl/src/plugins/dz-server/methods/docker/index.sh
@@ -2,19 +2,21 @@
 
 source $DZ_TOOL_PATH
 
-ARGS=$(getopt -l xxx:,xxx2: -n 'dzctl' -- "$@")
+ARGS=$(getopt -l web: -n 'dzctl' -- "$@")
 [ $? != 0 ] && echo Erro options && exit
 eval set -- "${ARGS}"
 while true; do
-    case $1 in
-    --xxx) ;;
-    --)
-        break
-        ;;
-    *)
-        dzLogError "Internal error!" && exit
-        ;;
-    esac
+  case $1 in
+  --web)
+    WebMode=$1 && shift 2
+    ;;
+  --)
+    break
+    ;;
+  *)
+    dzLogError "Internal error!" && exit
+    ;;
+  esac
 done
 
 ###################################################################################################
@@ -23,5 +25,32 @@ done
 
 StageNo=1
 
-logStage $StageNo "First Stage Description"
+logStage $StageNo "安装 Docker"
+dzRpm docker-ce
+systemctl enable --now docker
+DaemonJson=/etc/docker/daemon.json
+dzTmpFsPush $DaemonJson &&
+  dzTmpFsPull $DaemonJson
+systemctl daemon-reload
+systemctl restart docker
 let StageNo+=1
+
+logStage $StageNo "安装 Docker Dashboard Web"
+if [[ ! $WebMode = 0 ]]; then
+  DzDockerDashboardWebDC=/etc/dz/docker-compose/dz-docker-dashboard-web/docker-compose.yml
+  dzTmpFsPush $DzDockerDashboardWebDC &&
+    dzTmpFsPull $DzDockerDashboardWebDC
+  docker pull joinsunsoft/docker.ui &&
+    docker tag joinsunsoft/docker.ui dz-dashboard-docker-ui:1.0.0
+  docker pull portainer/portainer-ce &&
+    docker tag portainer/portainer-ce dz-dashboard-portainer-ce:1.0.0
+  docker compose -f $DzDockerDashboardWebDC up -d
+  docker rmi joinsunsoft/docker.ui
+  docker rmi portainer/portainer-ce
+  dzLogInfo "[访问] 192.168.226.100:9001"
+  dzLogInfo "[访问] 192.168.226.100:9002"
+  let StageNo+=1
+else
+  dzLogInfo "不部署 Docker Dashboard Web"
+  let StageNo+=1
+fi
