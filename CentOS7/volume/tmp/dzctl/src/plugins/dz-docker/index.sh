aaff1ed795814c34c4706f3f3add388f62e28c26
Dylan Zhang <987883389@qq.com>
2023-05-01 12:57:23 +0800

    chore

--- CentOS7/volume/tmp/dzctl/src/plugins/dz-docker/index.sh
+++ CentOS7/volume/tmp/dzctl/src/plugins/dz-docker/index.sh
@@ -2,7 +2,7 @@
 
 PluginCode=docker
 
-MethodDirPath=$DZ_CLOUD_PATH/cloud-file/CentOS7/volume/tmp/dzctl/src/plugins/dz-$PluginCode/methods
+MethodDirPath=$DZ_CTL_PATH/src/plugins/dz-$PluginCode/methods
 MethodCode=$1
 Argument=${*/$1/}
 source $MethodDirPath/$MethodCode/index.sh ArgHolder $Argument
