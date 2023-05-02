9e0c9be0a5c3acff8a1909d60c68c8ad4dc661b0
Dylan Zhang <987883389@qq.com>
2023-05-02 19:21:26 +0800

    chore

--- install.sh
+++ install.sh
@@ -1,5 +1,23 @@
 #!/bin/bash -i
 
+DzCloudPath=$1
+DzCtlPath=$DzCloudPath/cloud-file/CentOS7/volume/tmp/dzctl
+DzAdmPath=$DzCloudPath/cloud-file/CentOS7/volume/tmp/dzadm
+DzToolPath=$DzCloudPath/cloud-file/CentOS7/volume/tmp/dztool
+DzTmpFsPath=$DzCloudPath/.tmpfs
+DzBakFsPath=$DzCloudPath/.bakfs
+DzVolFsPath=$DzCloudPath/.volfs
+
+DZ_CLOUD_PATH=$DzCloudPath
+DZ_TMP_FS_PATH=$DzTmpFsPath
+DZ_BAK_FS_PATH=$DzBakFsPath
+DZ_VOL_FS_PATH=$DzVolFsPath
+
+[[ ! $DzCloudPath =~ ^\/ ]] && echo "DzCloudPath is invalid" && exit 0
+mkdir -p $DzCloudPath
+mkdir -p $DzTmpFsPath
+mkdir -p $DzBakFsPath
+
 ###################################################################################################
 ## 日志模块 dz-log
 ###################################################################################################
