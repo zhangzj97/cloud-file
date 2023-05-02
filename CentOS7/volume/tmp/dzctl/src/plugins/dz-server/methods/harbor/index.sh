7f75530b9543b1aae1bd4e271fa4f05f5db3d684
Dylan Zhang <987883389@qq.com>
2023-05-02 21:22:10 +0800

    chore

--- CentOS7/volume/tmp/dzctl/src/plugins/dz-server/methods/harbor/index.sh
+++ CentOS7/volume/tmp/dzctl/src/plugins/dz-server/methods/harbor/index.sh
@@ -2,19 +2,19 @@
 
 source $DZ_TOOL_PATH
 
-ARGS=$(getopt -l domain:,port:,ssl:, -n 'dzctl' -- "$@")
+ARGS=$(getopt -l domain:,port:, -n 'dzctl' -- "$@")
 [ $? != 0 ] && echo Erro options && exit
 eval set -- "${ARGS}"
 while true; do
   case $1 in
   --domain)
-    DomainNew=$2 && shift 2
+    Domain=$2 && shift 2
     ;;
   --port)
-    PortNew=$2 && shift 2
+    Port=$2 && shift 2
     ;;
   --ssl)
-    SSLNew=$2 && shift 2
+    SSL=$2 && shift 2
     ;;
   --)
     break
@@ -24,23 +24,32 @@ while true; do
     ;;
   esac
 done
-[[ ! $DomainNew ]] && logErrorResult "option --domain is invalid" && exit 0
-[[ ! $PortNew ]] && logErrorResult "option --port is invalid" && exit 0
+[[ ! $Domain ]] && logErrorResult "option --domain is invalid" && exit 0
+[[ ! $Port ]] && logErrorResult "option --port is invalid" && exit 0
 
 StageNo=0
 
-logStage $StageNo "Install Harbor"
-ServerDomainPort=$DomainNew:$PortNew
-[[ $SSLNew ]] && ServerDomainPort=$SSLNew
-DzCertsdPath=/etc/dz/certs.d
-ServerCertsdPath=$DzCertsdPath/$ServerDomainPort
-[[ ! -d $ServerCertsdPath ]] && logErrorResult "No $ServerCertsdPath" && exit
-DzHarborYmlTmpl=$DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/harbor-installer/dz-harbor.yml.tmpl
-DzHarborYml=$DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/harbor-installer/dz-harbor.yml
-/bin/cp -fa $DzHarborYmlTmpl $DzHarborYml && logFile $DzHarborYml
-sed -i "s/__hostname__/$DomainNew/" $DzHarborYml
-sed -i "s/__https_port__/${PortNew}/" $DzHarborYml
-sed -i "s#__https_certificate__#${ServerCertsdPath}/server.cert#" $DzHarborYml
-sed -i "s#__https_private_key__#${ServerCertsdPath}/server.key#" $DzHarborYml
-source $DZ_CLOUD_PATH/cloud-file/CentOS7/volume/etc/dz/harbor-installer/install.sh &&
-  logStep "$DomainNew:$PortNew ==> "
+dzLogStage $StageNo "检查 Harbor"
+ServerDomainPort=$Domain:$Port
+ServerKey=/etc/docker/certs.d/$ServerDomainPort/server.key
+ServerCert=/etc/docker/certs.d/$ServerDomainPort/server.cert
+[[ ! -f $ServerKey ]] && dzLogError "File $ServerKey is not found" && exit
+dzLogInfo "准备 Harbor 安装文件"
+DzHarborInstallerFile01=$DZ_VOL_FS_PATH/etc/dz/harbor-installer/install.sh
+DzHarborInstallerFile02=$DZ_VOL_FS_PATH/etc/dz/harbor-installer/common.sh
+DzHarborInstallerFile03=$DZ_VOL_FS_PATH/etc/dz/harbor-installer/prepare
+DzHarborInstallerFile04=$DZ_VOL_FS_PATH/etc/dz/harbor-installer/harbor.yml
+dzTmpFsPush $DzHarborInstallerFile01 && dzTmpFsPull $DzHarborInstallerFile01
+dzTmpFsPush $DzHarborInstallerFile02 && dzTmpFsPull $DzHarborInstallerFile02
+dzTmpFsPush $DzHarborInstallerFile03 && dzTmpFsPull $DzHarborInstallerFile03
+dzTmpFsPush $DzHarborInstallerFile04 && dzTmpFsPull $DzHarborInstallerFile04
+dzLogInfo "修改 Harbor env"
+
+DzHarborEnv=$DZ_VOL_FS_PATH/etc/dz/harbor-installer/.env
+dzTmpFsPush $DzHarborYml &&
+  dzTmpFsEdit $DzHarborYml 's|^.*hostname="?([^"]*)"?.*$|hostname=${Domain}|g' &&
+  dzTmpFsEdit $DzHarborYml 's|^.*https_port="?([^"]*)"?.*$|https_port=${Port}|g' &&
+  dzTmpFsEdit $DzHarborYml 's|^.*https_private_key="?([^"]*)"?.*$|https_private_key=${ServerKey}|g' &&
+  dzTmpFsEdit $DzHarborYml 's|^.*https_certificate="?([^"]*)"?.*$|https_certificate=${ServerCert}|g' &&
+  dzTmpFsPull $DzHarborYml
+source /etc/dz/harbor-installer/install.sh
