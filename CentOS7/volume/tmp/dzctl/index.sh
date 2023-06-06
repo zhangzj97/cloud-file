11f7113894bb2026d76a588911ce4cd14b2d5264
Dylan Zhang <987883389@qq.com>
2023-06-06 09:17:45 +0800

    chore

--- CentOS7/volume/tmp/dzctl/index.sh
+++ CentOS7/volume/tmp/dzctl/index.sh
@@ -2,8 +2,6 @@
 
 PluginDirPath=$DZ_CTL_PATH/src/plugins
 PluginCode=$1
-MethodCode=$2
 Argument=${*/$1/}
-[[ ! -f $PluginDirPath/dz-$PluginCode/index.sh ]] && echo Error PluginCode && exit
-[[ ! -f $PluginDirPath/dz-$PluginCode/methods/$MethodCode/index.sh ]] && echo Error MethodCode && exit
-source $PluginDirPath/dz-$PluginCode/index.sh $Argument
+[[ ! -f $PluginDirPath/$PluginCode/index.sh ]] && echo Error PluginCode && exit
+source $PluginDirPath/$PluginCode/index.sh $Argument
