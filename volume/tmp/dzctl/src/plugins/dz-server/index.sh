ffe97d42c2bd39c6516c9826bf63409957b0bd93
Dylan Zhang <987883389@qq.com>
2023-06-06 10:40:35 +0800

    chore

--- volume/tmp/dzctl/src/plugins/dz-server/index.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -i
-
-PluginCode=server
-
-MethodDirPath=$DZ_CTL_PATH/src/plugins/dz-$PluginCode/methods
-MethodCode=$1
-Argument=${*/$1/}
-source $MethodDirPath/$MethodCode/index.sh ArgHolder $Argument
